207329b5a270443318c05196ef9d77aece2f77c9
Anton Korotkov <akorotkov@aerospike.com>
2023-10-23 18:21:04 +0300

    add @ID annotation and response codes

--- internal/server/system_handlers.go
+++ internal/server/system_handlers.go
@@ -8,9 +8,10 @@ import (
 )
 
 // @Summary     Root endpoint.
+// @ID			root
 // @Tags        System
 // @Router      / [get]
-// @Success 	200 ""
+// @Success 	200
 func rootActionHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		w.WriteHeader(http.StatusNotFound)
@@ -19,22 +20,25 @@ func rootActionHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // @Summary     Health endpoint.
+// @ID			health
 // @Tags        System
 // @Router      /health [get]
-// @Success 	200  "Ok"
+// @Success 	200
 func healthActionHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "Ok")
 }
 
 // @Summary     Readiness endpoint.
+// @ID			ready
 // @Tags        System
 // @Router      /ready [get]
-// @Success 	200  "Ok"
+// @Success 	200
 func readyActionHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "Ok")
 }
 
 // @Summary     Returns application version.
+// @ID			version
 // @Tags        System
 // @Router      /version [get]
 // @Success 	200 {string} string "version"
