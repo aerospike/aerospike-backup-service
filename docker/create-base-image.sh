35a3f43d199efb6a7a75380b2739e112edc398fe
yrizhkov <yrizhkov@aerospike.com>
2023-10-26 17:14:28 +0300

    remove build scripts

--- docker/create-base-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker buildx create --use --name mybuilder
-docker login aerospike.jfrog.io -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker buildx build -t aerospike.jfrog.io/ecosystem-container-dev-local/aerospike-backup-service-base:latest \
- --push --platform linux/amd64,linux/arm64 \
- --file ./docker/Dockerfile.base .
-docker buildx rm mybuilder
