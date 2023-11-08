df96dd7aca764e273f46831ec6a94ea47f3da123
Anton Korotkov <akorotkov@aerospike.com>
2023-11-08 13:56:05 +0200

    add logs

--- pkg/util/log.go
+++ pkg/util/log.go
@@ -13,14 +13,17 @@ var libLogRegex = regexp.MustCompile(`^(.+)\s\[(\D+)\]\s\[(\d+)\]\s(.*)$`)
 
 // LogCaptured logs the captured std output from the shared libraries.
 func LogCaptured(out string) {
-	slog.Debug(fmt.Sprintf("Start log capture:\n\n\n%s\n\n\n", out))
+	slog.Debug("Start log capture:")
+	slog.Debug(out)
 	if out == "" {
 		slog.Debug("No logs captured")
 		return
 	}
 	entries := strings.Split(strings.ReplaceAll(out, "\r\n", "\n"), "\n")
 	for _, entry := range entries {
-		if groups := libLogRegex.FindStringSubmatch(entry); len(groups) == 5 {
+		slog.Debug("| " + entry)
+		groups := libLogRegex.FindStringSubmatch(entry)
+		if len(groups) == 5 {
 			switch groups[2] {
 			case "ERR":
 				slog.Error(groups[4])
@@ -31,6 +34,8 @@ func LogCaptured(out string) {
 			}
 		} else { // print to stderr
 			fmt.Fprintln(os.Stderr, entry)
+			slog.Error("Cannot parse" + entry)
 		}
 	}
+	slog.Debug("End log capture")
 }
