37da0e8a73307ad74ecd6176a630e6411fb2a3d3
Anton Korotkov <akorotkov@aerospike.com>
2023-11-15 11:15:24 +0200

    remove unused

--- pkg/util/utils.go
+++ pkg/util/utils.go
@@ -1,25 +1,9 @@
 //nolint:typecheck
 package util
 
-import (
-	"github.com/aerospike/backup/pkg/model"
-)
-
 // Ptr returns a pointer to the given object.
 func Ptr[T any](obj T) *T {
 	return &obj
-}
-
-func GetByName[T model.WithName](collection []T, name *string) (int, T) {
-	if name != nil {
-		for i, item := range collection {
-			if item.GetName() != nil && *item.GetName() == *name {
-				return i, item
-			}
-		}
-	}
-	var zero T
-	return -1, zero
 }
 
 func Find[T any](collection map[string]T, f func(T) bool) (T, bool) {
