d1092ced9f67ef201c37bb42466a6f369c86ad60
Eugene R <yrizhkov@aerospike.com>
2023-11-23 15:41:12 +0200

    Mock CgoStdio to resolve occasional test failures (#60)

---
+++ pkg/stdio/cgo_stdio_mock.go
@@ -0,0 +1,11 @@
+//go:build ci
+
+package stdio
+
+type CgoStdio struct {
+}
+
+// Capture mocks the original call by returning an empty string.
+func (c *CgoStdio) Capture(f func()) string {
+	return ""
+}
