cea3e7a4f4f1e6c912ad6dc50a377eadb642da57
Anton Korotkov <akorotkov@aerospike.com>
2023-12-03 14:17:33 +0200

    configs

--- pkg/model/config.go
+++ pkg/model/config.go
@@ -2,8 +2,6 @@ package model
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
 	"log/slog"
 	"os"
 	"sync"
@@ -37,21 +35,6 @@ func NewConfigWithDefaultValues() *Config {
 	return config
 }
 
-// Validate validates the configuration.
-func (c *Config) Validate() error {
-	for _, routine := range c.BackupRoutines {
-		if err := routine.Validate(); err != nil {
-			return err
-		}
-	}
-	for _, storage := range c.Storage {
-		if err := storage.Validate(); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 // String satisfies the fmt.Stringer interface.
 func (c Config) String() string {
 	cfg, err := json.Marshal(c)
@@ -79,7 +62,6 @@ type RateLimiterConfig struct {
 type AerospikeCluster struct {
 	pwdOnce              sync.Once
 	pwd                  *string
-	Name                 *string `yaml:"name,omitempty" json:"name,omitempty"`
 	Host                 *string `yaml:"host,omitempty" json:"host,omitempty"`
 	Port                 *int32  `yaml:"port,omitempty" json:"port,omitempty"`
 	UseServicesAlternate *bool   `yaml:"use-services-alternate,omitempty" json:"use-services-alternate,omitempty"`
@@ -109,20 +91,8 @@ func (c *AerospikeCluster) GetPassword() *string {
 	return c.pwd
 }
 
-// Validate validates the Aerospike cluster entity.
-func (c *AerospikeCluster) Validate() error {
-	if c.Host == nil {
-		return errors.New("host is not specified")
-	}
-	if c.Port == nil {
-		return errors.New("port is not specified")
-	}
-	return nil
-}
-
 // Storage represents the configuration for a backup storage details.
 type Storage struct {
-	Name               *string      `yaml:"name,omitempty" json:"name,omitempty"`
 	Type               *StorageType `yaml:"type,omitempty" json:"type,omitempty"`
 	Path               *string      `yaml:"path,omitempty" json:"path,omitempty"`
 	S3Region           *string      `yaml:"s3-region,omitempty" json:"s3-region,omitempty"`
@@ -131,45 +101,28 @@ type Storage struct {
 	S3LogLevel         *string      `yaml:"s3-log-level,omitempty" json:"s3-log-level,omitempty"`
 }
 
-// Validate validates the storage configuration.
-func (s *Storage) Validate() error {
-	if s.Name == nil || *s.Name == "" {
-		return errors.New("storage name is required")
-	}
-	if s.Type == nil {
-		return errors.New("storage type is required")
-	}
-	if s.Path == nil {
-		return errors.New("storage path is required")
-	}
-	return nil
-}
-
 // BackupPolicy represents a scheduled backup policy.
 type BackupPolicy struct {
-	Name             *string     `yaml:"name,omitempty" json:"name,omitempty"`
-	BackupType       *BackupType `yaml:"type,omitempty" json:"type,omitempty"`
-	Parallel         *int32      `yaml:"parallel,omitempty" json:"parallel,omitempty"`
-	SocketTimeout    *uint32     `yaml:"socket-timeout,omitempty" json:"socket-timeout,omitempty"`
-	TotalTimeout     *uint32     `yaml:"total-timeout,omitempty" json:"total-timeout,omitempty"`
-	MaxRetries       *uint32     `yaml:"max-retries,omitempty" json:"max-retries,omitempty"`
-	RetryDelay       *uint32     `yaml:"retry-delay,omitempty" json:"retry-delay,omitempty"`
-	RemoveFiles      *bool       `yaml:"remove-files,omitempty" json:"remove-files,omitempty"`
-	RemoveArtifacts  *bool       `yaml:"remove-artifacts,omitempty" json:"remove-artifacts,omitempty"`
-	NoBins           *bool       `yaml:"no-bins,omitempty" json:"no-bins,omitempty"`
-	NoRecords        *bool       `yaml:"no-records,omitempty" json:"no-records,omitempty"`
-	NoIndexes        *bool       `yaml:"no-indexes,omitempty" json:"no-indexes,omitempty"`
-	NoUdfs           *bool       `yaml:"no-udfs,omitempty" json:"no-udfs,omitempty"`
-	Bandwidth        *uint64     `yaml:"bandwidth,omitempty" json:"bandwidth,omitempty"`
-	MaxRecords       *uint64     `yaml:"max-records,omitempty" json:"max-records,omitempty"`
-	RecordsPerSecond *uint32     `yaml:"records-per-second,omitempty" json:"records-per-second,omitempty"`
-	FileLimit        *uint64     `yaml:"file-limit,omitempty" json:"file-limit,omitempty"`
-	FilterExp        *string     `yaml:"filter-exp,omitempty" json:"filter-exp,omitempty"`
+	Parallel         *int32  `yaml:"parallel,omitempty" json:"parallel,omitempty"`
+	SocketTimeout    *uint32 `yaml:"socket-timeout,omitempty" json:"socket-timeout,omitempty"`
+	TotalTimeout     *uint32 `yaml:"total-timeout,omitempty" json:"total-timeout,omitempty"`
+	MaxRetries       *uint32 `yaml:"max-retries,omitempty" json:"max-retries,omitempty"`
+	RetryDelay       *uint32 `yaml:"retry-delay,omitempty" json:"retry-delay,omitempty"`
+	RemoveFiles      *bool   `yaml:"remove-files,omitempty" json:"remove-files,omitempty"`
+	RemoveArtifacts  *bool   `yaml:"remove-artifacts,omitempty" json:"remove-artifacts,omitempty"`
+	NoBins           *bool   `yaml:"no-bins,omitempty" json:"no-bins,omitempty"`
+	NoRecords        *bool   `yaml:"no-records,omitempty" json:"no-records,omitempty"`
+	NoIndexes        *bool   `yaml:"no-indexes,omitempty" json:"no-indexes,omitempty"`
+	NoUdfs           *bool   `yaml:"no-udfs,omitempty" json:"no-udfs,omitempty"`
+	Bandwidth        *uint64 `yaml:"bandwidth,omitempty" json:"bandwidth,omitempty"`
+	MaxRecords       *uint64 `yaml:"max-records,omitempty" json:"max-records,omitempty"`
+	RecordsPerSecond *uint32 `yaml:"records-per-second,omitempty" json:"records-per-second,omitempty"`
+	FileLimit        *uint64 `yaml:"file-limit,omitempty" json:"file-limit,omitempty"`
+	FilterExp        *string `yaml:"filter-exp,omitempty" json:"filter-exp,omitempty"`
 }
 
 // BackupRoutine represents a scheduled backup operation routine.
 type BackupRoutine struct {
-	Name          string `yaml:"name,omitempty" json:"name,omitempty"`
 	BackupPolicy  string `yaml:"backup-policy,omitempty" json:"backup-policy,omitempty"`
 	SourceCluster string `yaml:"source-cluster,omitempty" json:"source-cluster,omitempty"`
 	Storage       string `yaml:"storage,omitempty" json:"storage,omitempty"`
@@ -184,33 +137,6 @@ type BackupRoutine struct {
 
 	PartitionList *string `yaml:"partition-list,omitempty" json:"partition-list,omitempty"`
 	AfterDigest   *string `yaml:"after-digest,omitempty" json:"after-digest,omitempty"`
-}
-
-// Validate validates the backup routine configuration.
-func (r *BackupRoutine) Validate() error {
-	if r.Name == "" {
-		return routineValidationError("name")
-	}
-	if r.BackupPolicy == "" {
-		return routineValidationError("backup-policy")
-	}
-	if r.SourceCluster == "" {
-		return routineValidationError("source-cluster")
-	}
-	if r.Storage == "" {
-		return routineValidationError("storage")
-	}
-	if r.Namespace == nil {
-		return routineValidationError("namespace")
-	}
-	if r.IntervalMillis == nil && r.IncrIntervalMillis == nil {
-		return errors.New("interval or incr-interval must be specified for backup routine")
-	}
-	return nil
-}
-
-func routineValidationError(field string) error {
-	return fmt.Errorf("%s specification for backup routine is required", field)
 }
 
 // Clone clones the backup policy struct.
@@ -235,11 +161,4 @@ type StorageType int
 const (
 	Local StorageType = iota
 	S3
-)
-
-type BackupType int
-
-const (
-	Full BackupType = iota
-	Incremental
 )
