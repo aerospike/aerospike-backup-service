1260c566e259cfbcb4c3a31c3a95c3bfd4638ed3
Anton Korotkov <akorotkov@aerospike.com>
2024-02-01 16:08:23 +0200

    update swagger doc

--- internal/server/backup_handlers.go
+++ internal/server/backup_handlers.go
@@ -36,6 +36,8 @@ func (ws *HTTPServer) getAvailableFullBackups(w http.ResponseWriter, r *http.Req
 // @Tags     Backup
 // @Produce  json
 // @Param    name query string false "Backup routine name"
+// @Param    from query int false "Lower bound timestamp filter" format(int64)
+// @Param    to query int false "Upper bound timestamp filter" format(int64)
 // @Router   /backup/incremental/list [get]
 // @Success  200 {object} map[string][]model.BackupDetails "Incremental backups by routine"
 // @Failure  404 {string} string ""
