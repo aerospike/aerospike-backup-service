ebcebdfb376bb8f93f5ece7f3e056a9c03d1a428
Anton Korotkov <akorotkov@aerospike.com>
2024-02-12 10:02:57 +0200

    private property

--- pkg/service/configuration_manager_s3.go
+++ pkg/service/configuration_manager_s3.go
@@ -8,7 +8,7 @@ import (
 // performing I/O operations on AWS S3.
 type S3ConfigurationManager struct {
 	*S3Context
-	FilePath string
+	configFilePath string
 }
 
 var _ ConfigurationManager = (*S3ConfigurationManager)(nil)
@@ -17,20 +17,20 @@ var _ ConfigurationManager = (*S3ConfigurationManager)(nil)
 func NewS3ConfigurationManager(configStorage *model.Storage) ConfigurationManager {
 	s3Context, _ := NewS3Context(configStorage)
 	return &S3ConfigurationManager{
-		S3Context: s3Context,
-		FilePath:  *configStorage.Path,
+		S3Context:      s3Context,
+		configFilePath: *configStorage.Path,
 	}
 }
 
 // ReadConfiguration reads and returns the configuration from S3.
 func (s *S3ConfigurationManager) ReadConfiguration() (*model.Config, error) {
 	config := model.NewConfigWithDefaultValues()
-	_ = s.readFile(s.FilePath, config)
+	_ = s.readFile(s.configFilePath, config)
 	err := config.Validate()
 	return config, err
 }
 
 // WriteConfiguration writes the configuration to S3.
 func (s *S3ConfigurationManager) WriteConfiguration(config *model.Config) error {
-	return s.writeYaml(s.FilePath, config)
+	return s.writeYaml(s.configFilePath, config)
 }
