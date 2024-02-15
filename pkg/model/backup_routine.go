b1b1f4657c2ac0cf8edfbc0ba97db8e98a68bf14
Anton Korotkov <akorotkov@aerospike.com>
2024-02-15 11:50:50 +0200

    backup routine

--- pkg/model/backup_routine.go
+++ pkg/model/backup_routine.go
@@ -9,34 +9,34 @@ import (
 // BackupRoutine represents a scheduled backup operation routine.
 // @Description BackupRoutine represents a scheduled backup operation routine.
 type BackupRoutine struct {
-	// The name of the corresponding bakup policy.
-	BackupPolicy string `yaml:"backup-policy,omitempty" json:"backup-policy,omitempty"`
+	// The name of the corresponding backup policy.
+	BackupPolicy string `yaml:"backup-policy,omitempty" json:"backup-policy,omitempty" example:"daily" validate:"required"`
 	// The name of the corresponding source cluster.
-	SourceCluster string `yaml:"source-cluster,omitempty" json:"source-cluster,omitempty"`
+	SourceCluster string `yaml:"source-cluster,omitempty" json:"source-cluster,omitempty" example:"testCluster" validate:"required"`
 	// The name of the corresponding storage provider configuration.
-	Storage string `yaml:"storage,omitempty" json:"storage,omitempty"`
+	Storage string `yaml:"storage,omitempty" json:"storage,omitempty" example:"aws" validate:"required"`
 	// The Secret Agent configuration for the routine (optional).
-	SecretAgent *string `yaml:"secret-agent,omitempty" json:"secret-agent,omitempty"`
+	SecretAgent *string `yaml:"secret-agent,omitempty" json:"secret-agent,omitempty" example:"sa"`
 	// The interval for full backup as a cron expression string.
-	IntervalCron string `yaml:"interval-cron" json:"interval-cron"`
+	IntervalCron string `yaml:"interval-cron" json:"interval-cron" example:"0 0 * * * *" validate:"required"`
 	// The interval for incremental backup as a cron expression string (optional).
-	IncrIntervalCron string `yaml:"incr-interval-cron" json:"incr-interval-cron"`
+	IncrIntervalCron string `yaml:"incr-interval-cron" json:"incr-interval-cron" example:"*/10 * * * * *"`
 	// The list of the namespaces to back up (optional, empty list implies backup up whole cluster).
-	Namespaces []string `yaml:"namespaces,omitempty" json:"namespaces,omitempty"`
+	Namespaces []string `yaml:"namespaces,omitempty" json:"namespaces,omitempty" example:"source-ns1"`
 	// The list of backup set names (optional, an empty list implies backing up all sets).
-	SetList []string `yaml:"set-list,omitempty" json:"set-list,omitempty"`
+	SetList []string `yaml:"set-list,omitempty" json:"set-list,omitempty" example:"set1"`
 	// The list of backup bin names (optional, an empty list implies backing up all bins).
-	BinList []string `yaml:"bin-list,omitempty" json:"bin-list,omitempty"`
+	BinList []string `yaml:"bin-list,omitempty" json:"bin-list,omitempty" example:"dataBin"`
 	// The list of nodes in the Aerospike cluster to run the backup for.
 	NodeList []Node `yaml:"node-list,omitempty" json:"node-list,omitempty"`
 	// A list of Aerospike Server rack IDs to prefer when reading records for a backup.
-	PreferRacks []int `yaml:"prefer-racks,omitempty" json:"prefer-racks,omitempty"`
+	PreferRacks []int `yaml:"prefer-racks,omitempty" json:"prefer-racks,omitempty" example:"0"`
 
 	// Back up list of partition filters. Partition filters can be ranges, individual partitions,
 	// or records after a specific digest within a single partition.
 	// Default number of partitions to back up: 0 to 4095: all partitions.
-	PartitionList *string `yaml:"partition-list,omitempty" json:"partition-list,omitempty"`
-	AfterDigest   *string `yaml:"after-digest,omitempty" json:"after-digest,omitempty"`
+	PartitionList *string `yaml:"partition-list,omitempty" json:"partition-list,omitempty" example:"0-1000"`
+	AfterDigest   *string `yaml:"after-digest,omitempty" json:"after-digest,omitempty" example:"EjRWeJq83vEjRRI0VniavN7xI0U="`
 }
 
 // Validate validates the backup routine configuration.
@@ -76,6 +76,6 @@ func routineValidationError(field string) error {
 // Node represents the Aerospike node details.
 // @Description Node represents the Aerospike node details.
 type Node struct {
-	IP   string `yaml:"ip" json:"ip"`
-	Port int    `yaml:"port" json:"port"`
+	IP   string `yaml:"ip" json:"ip" example:"192.168.0.2"`
+	Port int    `yaml:"port" json:"port" example:"3000"`
 }
