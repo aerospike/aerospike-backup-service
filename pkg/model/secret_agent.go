b4374b912d33e1bba1ca48839d907fbcd83eceab
Anton Korotkov <akorotkov@aerospike.com>
2024-02-15 13:16:59 +0200

    bring back comments

--- pkg/model/secret_agent.go
+++ pkg/model/secret_agent.go
@@ -1,5 +1,12 @@
 package model
 
+// SecretAgent represents the configuration of an Aerospike Secret Agent
+// for a backup/restore operation.
+// Aerospike Secret Agent acts as a proxy layer between Aerospike server and one or more
+// external secrets management services, fetching secrets on behalf of the server.
+//
+// @Description SecretAgent represents the configuration of an Aerospike Secret Agent
+// @Description for a backup/restore operation.
 type SecretAgent struct {
 	// Address of the Secret Agent.
 	Address string `yaml:"address,omitempty" json:"address,omitempty" example:"localhost"`
