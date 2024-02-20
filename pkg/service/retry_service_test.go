e416d25379b0e01208eea3b043151758572e8c1b
Anton Korotkov <akorotkov@aerospike.com>
2024-02-20 11:56:11 +0200

    add test with double execution

--- pkg/service/retry_service_test.go
+++ pkg/service/retry_service_test.go
@@ -28,3 +28,27 @@ func Test_timer(t *testing.T) {
 		t.Errorf("Expected retryCounter 0, got %d", retryCounter)
 	}
 }
+
+func Test_timerRunTwice(t *testing.T) {
+	r := NewRetryService("test")
+	counterLock := sync.Mutex{}
+	retryCounter := 3
+	f := func() error {
+		counterLock.Lock()
+		defer counterLock.Unlock()
+		if retryCounter > 0 {
+			retryCounter--
+			return errors.New("mock error")
+		}
+		return nil
+	}
+	r.retry(f, time.Second, 3)
+	r.retry(f, time.Second, 3)
+
+	time.Sleep(5 * time.Second)
+	counterLock.Lock()
+	defer counterLock.Unlock()
+	if retryCounter != 0 {
+		t.Errorf("Expected retryCounter 0, got %d", retryCounter)
+	}
+}
