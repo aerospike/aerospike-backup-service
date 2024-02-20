f1a1646c04b28bd314c0c9a440f93f4f5915e16f
Anton Korotkov <akorotkov@aerospike.com>
2024-02-20 11:21:44 +0200

    add retry service test

--- pkg/service/retry_service_test.go
+++ pkg/service/retry_service_test.go
@@ -1,15 +1,23 @@
 package service
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 	"time"
 )
 
 func Test_timer(t *testing.T) {
 	r := NewRetryService("test")
+	retryCounter := 2
 	r.retry(func() error {
-		return fmt.Errorf("")
+		if retryCounter > 0 {
+			retryCounter--
+			return errors.New("mock error")
+		}
+		return nil
 	}, time.Second, 3)
-	time.Sleep(50 * time.Second)
+	time.Sleep(5 * time.Second)
+	if retryCounter != 0 {
+		t.Errorf("Expected retryCounter 0, got %d", retryCounter)
+	}
 }
