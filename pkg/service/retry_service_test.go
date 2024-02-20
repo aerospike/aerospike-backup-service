e364729a41d788bd1e78ac1ec5a6fdf21a5f10e3
Anton Korotkov <akorotkov@aerospike.com>
2024-02-20 11:38:37 +0200

    add lock for test

--- pkg/service/retry_service_test.go
+++ pkg/service/retry_service_test.go
@@ -2,21 +2,28 @@ package service
 
 import (
 	"errors"
+	"sync"
 	"testing"
 	"time"
 )
 
 func Test_timer(t *testing.T) {
 	r := NewRetryService("test")
+	counterLock := sync.Mutex{}
 	retryCounter := 2
 	r.retry(func() error {
+		counterLock.Lock()
+		defer counterLock.Unlock()
 		if retryCounter > 0 {
 			retryCounter--
 			return errors.New("mock error")
 		}
 		return nil
 	}, time.Second, 3)
+
 	time.Sleep(5 * time.Second)
+	counterLock.Lock()
+	defer counterLock.Unlock()
 	if retryCounter != 0 {
 		t.Errorf("Expected retryCounter 0, got %d", retryCounter)
 	}
