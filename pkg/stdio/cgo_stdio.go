727c12ebb8344e05b5d312305df13348c95cd1e9
Anton Korotkov <akorotkov@aerospike.com>
2024-03-03 12:47:06 +0200

    test other approach

--- pkg/stdio/cgo_stdio.go
+++ pkg/stdio/cgo_stdio.go
@@ -11,6 +11,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"log/slog"
 	"os"
 	"sync"
@@ -33,9 +34,43 @@ func NewCgoStdio(capture bool) *CgoStdio {
 // Stderr log capturer.
 var Stderr *CgoStdio
 
+func (c *CgoStdio) Capture(f func()) string {
+	c.Lock()
+	defer c.Unlock()
+
+	// don't capture shared library logs
+	if !c.capture {
+		f()
+		return ""
+	}
+
+	old := os.Stderr // keep backup of the real stdout
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal("Failed to create pipe:", err)
+	}
+	os.Stderr = w
+
+	f()
+
+	outC := make(chan string)
+	// copy the output in a separate goroutine so printing can't block indefinitely
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	// back to normal state
+	w.Close()
+	os.Stderr = old // restoring the real stdout
+	out := <-outC
+	return out
+}
+
 // Capture captures and returns the stderr output produced by the
 // given function f.
-func (c *CgoStdio) Capture(f func()) string {
+func (c *CgoStdio) CaptureOld(f func()) string {
 	c.Lock()
 	defer c.Unlock()
 
