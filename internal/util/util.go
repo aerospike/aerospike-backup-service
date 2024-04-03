749f91f21a283b4b6cec5d2542dd932ecf540dc7
Anton Korotkov <akorotkov@aerospike.com>
2024-04-03 10:49:41 +0300

    remove unused method

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log/slog"
@@ -80,43 +79,10 @@ func logLevel(level string) slog.Level {
 	}
 }
 
-// Check panics if the error is not nil.
-func Check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-// Returns an exit value for the error.
+// ToExitVal returns an exit value for the error.
 func ToExitVal(err error) int {
 	if err != nil {
 		return 1
 	}
 	return 0
-}
-
-// CaptureStdout returns the stdout output written during the
-// given function execution.
-func CaptureStdout(f func()) string {
-	old := os.Stdout // keep backup of the real stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	f() // run the function
-
-	outC := make(chan string)
-	// copy the output in a separate goroutine so printing
-	// can't block indefinitely
-	go func() {
-		var buf bytes.Buffer
-		_, err := io.Copy(&buf, r)
-		Check(err)
-		outC <- buf.String()
-	}()
-
-	// back to normal state
-	_ = w.Close()
-	os.Stdout = old // restoring the real stdout
-	out := <-outC
-	return out
 }
