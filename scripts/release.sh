e5bd01eb9850a5b993e49a29f15538c3e0f2b6b9
David Gerchikov <dgerchikov@aerospike.com>
2024-04-12 14:20:04 +0300

    Add release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,9 +4,7 @@ NEXT_VERSION="$1"
 PREV_VERSION="$(cat "$WORKSPACE"/VERSION)"
 CHANGE_LOG_RECORD_TEMP_FILE="$(mktemp)"
 CHANGE_LOG_TEMP_FILE="$(mktemp)"
-TEMP_FILE="$(mktemp)"
 
-sed -i "s/$PREV_VERSION/$NEXT_VERSION/g" "$WORKSPACE"/VERSION
 
 cat <<EOF > "$CHANGE_LOG_RECORD_TEMP_FILE"
 aerospike-backup-service ($NEXT_VERSION-1) unstable; urgency=low
@@ -17,10 +15,11 @@ EOF
 cat "$CHANGE_LOG_RECORD_TEMP_FILE" "$WORKSPACE"/packages/deb/debian/changelog > "$CHANGE_LOG_TEMP_FILE"
 mv "$CHANGE_LOG_TEMP_FILE" "$WORKSPACE"/packages/deb/debian/changelog
 
-tac "$WORKSPACE"/docs/docs.go | sed "s/$PREV_VERSION/$NEXT_VERSION" | tac > "$TEMP_FILE"
-mv "$TEMP_FILE" "$WORKSPACE"/docs/docs.go
+docker run --rm --interactive --volume "$WORKSPACE":/local bash:latest <<EOF
+sed -i "s/$PREV_VERSION/$NEXT_VERSION/g" /local/VERSION
+sed -i "s/\"$PREV_VERSION\"/\"$NEXT_VERSION\"/" /local/docs/docs.go
+sed -i "s/$PREV_VERSION/$NEXT_VERSION/" /local/internal/server/server.go
+sed -i "s/$PREV_VERSION/$NEXT_VERSION/" /local/internal/util/version.go
+EOF
 
-sed -i "s/$PREV_VERSION/$NEXT_VERSION" "$WORKSPACE"/internal/server/server.go
-sed -i "s/$PREV_VERSION/$NEXT_VERSION" "$WORKSPACE"/internal/util/version.go
-
-bash -c "$WORKSPACE"/scripts/generate_OpenApi.sh
+bash -c "$WORKSPACE"/scripts/generate-openapi.sh
