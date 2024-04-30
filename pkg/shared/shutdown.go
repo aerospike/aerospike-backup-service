a4250612365cd06c66c84cebe8851bc24a2f2a21
Anton Korotkov <akorotkov@aerospike.com>
2024-04-30 13:26:58 +0300

    delete old module

--- pkg/shared/shutdown.go
+++ pkg/shared/shutdown.go
@@ -2,16 +2,6 @@
 
 package shared
 
-/*
-#include <file_proxy.h>
-*/
-import "C"
-import (
-	"log/slog"
-)
-
 // Shutdown performs finalization operations on shared resources.
 func Shutdown() {
-	slog.Info("Finalizing shared resources")
-	C.file_proxy_cloud_shutdown()
 }
