d82b750c3f6ed1492c9601cc0db513fea19fc6c2
Anton Korotkov <akorotkov@aerospike.com>
2024-07-29 11:53:19 +0300

    merge

--- pkg/service/backup_handler.go
+++ pkg/service/backup_handler.go
@@ -180,7 +180,7 @@ func (h *BackupHandler) writeClusterConfiguration(client *aerospike.Client, now 
 	for i, info := range infos {
 		confFilePath := fmt.Sprintf("%s/aerospike_%d.conf", path, i)
 		slog.Debug("Write aerospike configuration", "path", confFilePath)
-		err = h.backend.write(confFilePath, []byte(info))
+		err := h.backend.write(confFilePath, []byte(info))
 		if err != nil {
 			slog.Error("Failed to write configuration for the backup", "name", h.routineName, "err", err)
 		}
