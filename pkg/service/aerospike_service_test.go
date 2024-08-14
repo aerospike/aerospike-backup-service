8e84ad1c58c5d77095c318d11a96e8c648801fae
akorotkov <akorotkov@aerospike.com>
2024-08-14 16:36:24 +0300

    reuse client in fetch namespace

--- pkg/service/aerospike_service_test.go
+++ pkg/service/aerospike_service_test.go
@@ -6,16 +6,15 @@ import (
 	"testing"
 
 	"github.com/aerospike/aerospike-backup-service/pkg/model"
+	as "github.com/aerospike/aerospike-client-go/v7"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test(t *testing.T) {
 	cluster := model.NewLocalAerospikeCluster()
-	namespaces, err := getAllNamespacesOfCluster(cluster)
-	if err != nil {
-		t.Fatalf("Expected error nil, got %v", err)
-	}
-
-	if len(namespaces) == 0 {
-		t.Fatalf("No namespaces found")
-	}
+	client, aerr := as.NewClientWithPolicyAndHost(cluster.ASClientPolicy(), cluster.ASClientHosts()...)
+	assert.NoError(t, aerr)
+	namespaces, err := getAllNamespacesOfCluster(client)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, namespaces)
 }
