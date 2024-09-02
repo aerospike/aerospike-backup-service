e9155e9c7100792308c3ba9b905a6dda2ddbfff3
akorotkov <akorotkov@aerospike.com>
2024-09-02 12:25:23 +0300

    add configuratin manager

---
+++ internal/server/configuration/configuration_manager_local.go
@@ -0,0 +1,68 @@
+package configuration
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/aerospike/aerospike-backup-service/internal/server/dto"
+	"github.com/aerospike/aerospike-backup-service/pkg/model"
+)
+
+// FileConfigurationManager implements the ConfigurationManager interface,
+// performing I/O operations on local storage.
+type FileConfigurationManager struct {
+	sync.Mutex
+	FilePath string
+}
+
+var _ ConfigurationManager = (*FileConfigurationManager)(nil)
+
+// NewFileConfigurationManager returns a new FileConfigurationManager.
+func NewFileConfigurationManager(path string) ConfigurationManager {
+	return &FileConfigurationManager{FilePath: path}
+}
+
+// ReadConfiguration returns a reader for the configuration file.
+func (cm *FileConfigurationManager) ReadConfiguration() (io.ReadCloser, error) {
+	cm.Lock()
+	defer cm.Unlock()
+
+	filePath := cm.FilePath
+	if filePath == "" {
+		return nil, errors.New("configuration file is missing")
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+// WriteConfiguration writes the configuration to the given file path.
+func (cm *FileConfigurationManager) WriteConfiguration(config *model.Config) error {
+	cm.Lock()
+	defer cm.Unlock()
+
+	filePath := cm.FilePath
+	if filePath == "" {
+		return errors.New("configuration file path is missing")
+	}
+
+	configDto := dto.NewConfigFromModel(config)
+	data, err := configDto.Serialize(dto.YAML)
+	if err != nil {
+		return fmt.Errorf("failed to marshal configuration data: %w", err)
+	}
+
+	err = os.WriteFile(filePath, data, 0600)
+	if err != nil {
+		return fmt.Errorf("failed to write configuration to file %q: %w", filePath, err)
+	}
+
+	return nil
+}
---
+++ internal/server/configuration/configuration_manager_s3.go
@@ -0,0 +1,32 @@
+package configuration
+
+import (
+	"io"
+
+	"github.com/aerospike/aerospike-backup-service/pkg/model"
+	"github.com/aerospike/aerospike-backup-service/pkg/service"
+)
+
+// S3ConfigurationManager implements the ConfigurationManager interface,
+// performing I/O operations on AWS S3.
+type S3ConfigurationManager struct {
+	*service.S3Context
+}
+
+var _ ConfigurationManager = (*S3ConfigurationManager)(nil)
+
+func newS3ConfigurationManager(configStorage *model.Storage) (ConfigurationManager, error) {
+	return &S3ConfigurationManager{
+		service.NewS3Context(configStorage),
+	}, nil
+}
+
+// ReadConfiguration reads and returns the configuration from S3.
+func (s *S3ConfigurationManager) ReadConfiguration() (io.ReadCloser, error) {
+	return s.Read(s.Path)
+}
+
+// WriteConfiguration writes the configuration to S3.
+func (s *S3ConfigurationManager) WriteConfiguration(config *model.Config) error {
+	return s.WriteYaml(s.Path, config)
+}
