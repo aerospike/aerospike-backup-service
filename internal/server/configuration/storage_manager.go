d385fff64057494c3f581227e0ebb6ecfe491c1d
akorotkov <akorotkov@aerospike.com>
2024-09-29 09:47:52 +0300

    add StorageManager docs

--- internal/server/configuration/storage_manager.go
+++ internal/server/configuration/storage_manager.go
@@ -11,11 +11,14 @@ import (
 	"github.com/aerospike/aerospike-backup-service/v2/pkg/service"
 )
 
+// StorageManager implement Manager interface.
+// it stores service configuration in provided Storage (Local, s3 aws etc.)
 type StorageManager struct {
 	storage model.Storage
 }
 
-func NewStorageManager(configStorage model.Storage) *StorageManager {
+// NewStorageManager returns new instance of StorageManager
+func NewStorageManager(configStorage model.Storage) Manager {
 	return &StorageManager{
 		storage: configStorage,
 	}
