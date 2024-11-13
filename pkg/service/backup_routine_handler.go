a73e035235d823432cbab4b002986761809716b6
akorotkov <akorotkov@aerospike.com>
2024-11-13 15:33:53 +0200

    don't add nil handler

--- pkg/service/backup_routine_handler.go
+++ pkg/service/backup_routine_handler.go
@@ -333,6 +333,7 @@ func (h *BackupRoutineHandler) startIncrementalBackupForAllNamespaces(
 			h.logger.Warn("could not start backup",
 				slog.String("namespace", namespace),
 				slog.Any("err", err))
+			continue
 		}
 		h.incrBackupHandlers[namespace] = handler
 	}
