11524b02dd7f54dd4ae5d23b4a6ca0cff1c9cd42
akorotkov <akorotkov@aerospike.com>
2024-12-05 19:29:51 +0200

    use %w

--- pkg/model/secret_agent.go
+++ pkg/model/secret_agent.go
@@ -55,7 +55,7 @@ func (s *SecretAgent) Read(path string) (string, error) {
 		readFromSecretAgentfunc := func(key string) (string, error) {
 			secret, err := backup.ParseSecret(agentConfig, key)
 			if err != nil {
-				return "", fmt.Errorf("failed to read secret %q from %s: %v", key, s.Address, err)
+				return "", fmt.Errorf("failed to read secret %q from %s: %w", key, s.Address, err)
 			}
 
 			return secret, nil
