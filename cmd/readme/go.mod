2c3399c192bdcec9c10de32b707fe4db4d6bb9b0
akorotkov <akorotkov@aerospike.com>
2024-12-08 14:27:00 +0200

    add readme

---
+++ cmd/readme/go.mod
@@ -0,0 +1,5 @@
+module cmd/readme
+
+go 1.23.3
+
+require github.com/aerospike/aerospike-backup-service v0.0.0-20240822110128-dc2b4811b9d3 // indirect
---
+++ cmd/readme/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/aerospike/aerospike-backup-service/v2/pkg/dto"
+	"os"
+	"regexp"
+)
+
+var jsonExamples = map[string]interface{}{}
+
+func main() {
+	_ = dto.AerospikeCluster{}
+	readme, err := os.ReadFile("/Users/ak/work/aerospike-backup-service/README.md")
+	if err != nil {
+		panic(err)
+	}
+
+	re := regexp.MustCompile("<!--\\s*(\\w+)\\s*-->\\s*```json[\\s\\S]*?```")
+	updatedReadme := re.ReplaceAllFunc(readme, func(match []byte) []byte {
+		submatches := re.FindSubmatch(match)
+		if len(submatches) < 2 {
+			return match
+		}
+
+		name := string(submatches[1])
+		example, exists := jsonExamples[name]
+		if !exists {
+			return match
+		}
+
+		formattedJSON, err := json.MarshalIndent(example, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+
+		var buffer bytes.Buffer
+		buffer.WriteString(fmt.Sprintf("Example: %s\n```json\n", name))
+		buffer.Write(formattedJSON)
+		buffer.WriteString("\n```")
+		return buffer.Bytes()
+	})
+
+	err = os.WriteFile("README.md", updatedReadme, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
