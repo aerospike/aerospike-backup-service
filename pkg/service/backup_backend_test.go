d138ea43f1d7852b1f65e978055b20c1ab5a3e1b
akorotkov <akorotkov@aerospike.com>
2024-12-21 23:01:16 +0200

    update backened test

--- pkg/service/backup_backend_test.go
+++ pkg/service/backup_backend_test.go
@@ -2,40 +2,25 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"github.com/stretchr/testify/require"
 	"os"
-	"strconv"
 	"testing"
 	"time"
 
 	"github.com/aerospike/aerospike-backup-service/v2/pkg/model"
 )
 
-func TestFullBackupRemoveFiles(t *testing.T) {
+func TestFullBackupReadFiles(t *testing.T) {
 	tempFolder := t.TempDir()
+	name := "routine"
 	backend := &BackupBackend{
 		storage:     &model.LocalStorage{Path: tempFolder},
-		routineName: "routine",
-	}
-
-	path := backend.routineName + "/backup/data/source-ns1/"
-	_ = os.MkdirAll(path, 0744)
-	_ = backend.writeBackupMetadata(context.Background(), path, model.BackupMetadata{Created: time.UnixMilli(10)})
-
-	to := model.NewTimeBoundsTo(time.UnixMilli(1000))
-	list, _ := backend.FullBackupList(context.Background(), to)
-	require.Equal(t, 1, len(list))
-}
-
-func TestFullBackupKeepFiles(t *testing.T) {
-	tempFolder := t.TempDir()
-	backend := &BackupBackend{
-		storage:     &model.LocalStorage{Path: tempFolder},
-		routineName: "routine",
+		routineName: name,
 	}
 
 	for _, t := range []int64{10, 20, 30} {
-		path := backend.routineName + "/backup/" + strconv.FormatInt(t, 10) + "/data/source-ns1/"
+		path := fmt.Sprintf("routine/backup/%d/data/source-ns1/", t)
 		_ = os.MkdirAll(path, 0744)
 		_ = backend.writeBackupMetadata(context.Background(), path, model.BackupMetadata{Created: time.UnixMilli(t)})
 	}
