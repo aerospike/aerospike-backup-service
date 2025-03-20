5dbacdc92d494676f762c3974fbb0ad46658df4b
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2025-03-20 16:01:24 +0200

    errors in doubpt

--- pkg/dto/restore_result.go
+++ pkg/dto/restore_result.go
@@ -14,14 +14,6 @@ const (
 // RestoreJobStatus represents a restore job status.
 // @Description RestoreJobStatus represents a restore job status.
 type RestoreJobStatus struct {
-	RestoreStats
-	CurrentRestore *RunningJob `yaml:"current-restore,omitempty" json:"current-job,omitempty"`
-	Status         JobStatus   `yaml:"status,omitempty" json:"status,omitempty"`
-	Error          string      `yaml:"error,omitempty" json:"error,omitempty"`
-}
-
-// RestoreStats represents the statistics of a restore operation.
-type RestoreStats struct {
 	ReadRecords     uint64 `yaml:"read-records,omitempty" json:"read-records,omitempty" format:"int64" example:"10"`
 	TotalBytes      uint64 `yaml:"total-bytes,omitempty" json:"total-bytes,omitempty" format:"int64" example:"2000"`
 	ExpiredRecords  uint64 `yaml:"expired-records,omitempty" json:"expired-records,omitempty" format:"int64" example:"2"`
@@ -32,6 +24,18 @@ type RestoreStats struct {
 	FresherRecords  uint64 `yaml:"fresher-records,omitempty" json:"fresher-records,omitempty" format:"int64" example:"5"`
 	IndexCount      uint64 `yaml:"index-count,omitempty" json:"index-count,omitempty" format:"int64" example:"3"`
 	UDFCount        uint64 `yaml:"udf-count,omitempty" json:"udf-count,omitempty" format:"int64" example:"1"`
+
+	// The number of errors in doubt while restoring.
+	// (IsInDoubt signifies that the write operation may have gone through on the server
+	// but the client is not able to confirm that due an error.)
+	// Non zero value indicates that there are might be unexpected side effects during restore, like
+	// * Generation counter greater than expected for some records.
+	// * Fresher records counter greater than expected.
+	ErrorsInDoubt uint64 `yaml:"errors-in-doubt,omitempty" json:"errors-in-doubt,omitempty" format:"int64" example:"7"`
+
+	CurrentRestore *RunningJob `yaml:"current-restore,omitempty" json:"current-job,omitempty"`
+	Status         JobStatus   `yaml:"status,omitempty" json:"status,omitempty"`
+	Error          string      `yaml:"error,omitempty" json:"error,omitempty"`
 }
 
 func NewResultFromModel(m *model.RestoreJobStatus) *RestoreJobStatus {
@@ -57,5 +61,6 @@ func (r *RestoreJobStatus) fromModel(m *model.RestoreJobStatus) {
 	r.UDFCount = m.UDFCount
 	r.Status = JobStatus(m.Status)
 	r.Error = m.Error
+	r.ErrorsInDoubt = m.ErrorsInDoubt
 	r.CurrentRestore = NewRunningJobFromModel(m.CurrentRestore)
 }
