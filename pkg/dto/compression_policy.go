5508d9ff29b497f0bb4ec18d50d84962d3ef17d4
akorotkov <akorotkov@aerospike.com>
2025-04-20 12:04:41 +0300

    don't update compression level

--- pkg/dto/compression_policy.go
+++ pkg/dto/compression_policy.go
@@ -17,7 +17,8 @@ const (
 type CompressionPolicy struct {
 	// The compression mode to be used (default is NONE).
 	Mode string `yaml:"mode,omitempty" json:"mode,omitempty" default:"NONE" enums:"NONE,ZSTD"`
-	// The compression level to use (or -1 if unspecified).
+	// The compression level to use.
+	// Algorithm-specific; for zstd: from -1 (fastest) to 22 (best compression).
 	Level int32 `yaml:"level,omitempty" json:"level,omitempty"`
 }
 
@@ -28,9 +29,6 @@ func (p *CompressionPolicy) Validate() error {
 	}
 	if p.Mode != CompressNone && p.Mode != CompressZSTD {
 		return fmt.Errorf("invalid compression mode: %s", p.Mode)
-	}
-	if p.Level == 0 {
-		p.Level = -1
 	}
 	if p.Level < -1 {
 		return fmt.Errorf("invalid compression level: %d", p.Level)
