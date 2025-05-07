35eb6d0cf0b4a5bc3024fc9f9478013b0ab23934
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2025-05-07 13:27:10 +0300

    APPS-1667 Add config JSON schema to allow autocomplete and real-time validation (#350)
    * add config schema json
    
    * add version
    
    * add schema header
    
    * add schema header
    
    * remove whitespaces

--- build/scripts/generate-openapi.sh
+++ build/scripts/generate-openapi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 WORKSPACE="$(git rev-parse --show-toplevel)"
 
+# generate swagger documentation using https://github.com/swaggo/swag tool
 docker run --rm --volume "$WORKSPACE":/local davi17g/swag:latest init \
 -d /local/internal/server/handlers,/local/pkg/dto -g info.go -o /local/docs
 
 # swag codegen cannot handle int64 format for return values
-
 cat <<< "$(docker run --rm --volume "$WORKSPACE/docs/swagger.json":/local/docs/swagger.json ghcr.io/jqlang/jq:latest \
 '(.paths.[].[].responses
 | select(has("202")).["202"]
@@ -22,6 +22,7 @@ cat <<< "$(docker run --rm --volume "$WORKSPACE/docs/swagger.yaml":/local/docs/s
 
 mkdir -p "$WORKSPACE"/tmp
 
+# convert swagger to open-api
 docker run --rm --volume "$WORKSPACE":/local openapitools/openapi-generator-cli:latest generate \
 -i /local/docs/swagger.yaml -g openapi-yaml -o /local/tmp
 
@@ -32,3 +33,14 @@ mv "$WORKSPACE"/tmp/openapi/openapi.yaml "$WORKSPACE"/docs/openapi.yaml
 mv "$WORKSPACE"/tmp/openapi.json "$WORKSPACE"/docs/openapi.json
 
 rm -rf "$WORKSPACE"/tmp "$WORKSPACE"/docs/swagger.yaml "$WORKSPACE"/docs/swagger.json
+
+# generate config schema
+jq '{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": .components.schemas["dto.Config"].properties,
+  "required": .components.schemas["dto.Config"].required,
+  "components": {
+    "schemas": ( .components.schemas | del(.["dto.Config"]) )
+  }
+}' "$WORKSPACE"/docs/openapi.json > "$WORKSPACE"/docs/config.schema.json
