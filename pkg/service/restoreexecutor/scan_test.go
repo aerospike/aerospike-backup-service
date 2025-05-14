604c245b2f1edae1f05f369c9441ca295e655d69
Anton Korotkov <akorotkov@aerospike.com>
2025-05-14 09:15:04 +0300

    linter

--- pkg/service/restoreexecutor/scan_test.go
+++ pkg/service/restoreexecutor/scan_test.go
@@ -1,11 +1,11 @@
 package restoreexecutor
 
 import (
-	as "github.com/aerospike/aerospike-client-go/v8"
 	"testing"
 
 	"github.com/aerospike/aerospike-backup-service/v3/pkg/model"
 	"github.com/aerospike/aerospike-backup-service/v3/pkg/util"
+	as "github.com/aerospike/aerospike-client-go/v8"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
