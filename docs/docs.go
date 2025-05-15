7f460335efbdc7d2c5bca8f6c104da203d7dd120
David Gerchikov <dgerchikov@aerospike.com>
2025-05-15 17:00:17 +0300

    Release: v3.1.0

--- docs/docs.go
+++ docs/docs.go
@@ -3093,7 +3093,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "v3.0.0",
+	Version:          "v3.1.0",
 	Host:             "localhost:8080",
 	BasePath:         "",
 	Schemes:          []string{},
