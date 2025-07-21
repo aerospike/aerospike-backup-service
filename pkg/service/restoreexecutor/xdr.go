4f0315e2cd5b6be335c9049a4a6cc0ffefb927fa
Dmitrii Neeman <dneeman@aerospike.com>
2025-07-21 16:17:38 +0300

    FMWK-788-new-limiter - removed unused casting

--- pkg/service/restoreexecutor/xdr.go
+++ pkg/service/restoreexecutor/xdr.go
@@ -57,7 +57,7 @@ func makeXdrRestoreConfig(restoreRequest *model.RestoreRequest,
 	}
 
 	config.RecordsPerSecond = util.ValueOrZero(restoreRequest.Policy.Tps)
-	config.Bandwidth = int64(util.ValueOrZero(restoreRequest.Policy.Bandwidth))
+	config.Bandwidth = util.ValueOrZero(restoreRequest.Policy.Bandwidth)
 	config.Parallel = restoreRequest.Policy.GetParallelOrDefault()
 	config.MaxAsyncBatches = restoreRequest.Policy.GetMaxAsyncBatchesOrDefault()
 	config.BatchSize = restoreRequest.Policy.GetBatchSizeOrDefault()
