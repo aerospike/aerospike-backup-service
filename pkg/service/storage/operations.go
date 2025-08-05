38c61ba9724807d617b89d2bfafba0973616d78c
Anton Korotkov <akorotkov@aerospike.com>
2025-08-05 14:13:47 +0300

    fix logs

--- pkg/service/storage/operations.go
+++ pkg/service/storage/operations.go
@@ -133,7 +133,7 @@ func ReadFileNames(
 		startScanFrom = filepath.Join(storage.GetPath(), path, fromTimeStr)
 	}
 
-	slog.Info("Reading file names from %s", startScanFrom)
+	slog.Info("Reading file names from", slog.String("path", startScanFrom))
 	reader, err := CreateDirReader(ctx, storage, path,
 		ioStorage.WithValidator(newNameValidator(filterStr)),
 		ioStorage.WithStartAfter(startScanFrom),
@@ -144,7 +144,7 @@ func ReadFileNames(
 		return nil, fmt.Errorf("failed to create reader: %w", err)
 	}
 
-	slog.Info("Start listing file names from %s", startScanFrom)
+	slog.Info("Start listing file names from", slog.String("path", startScanFrom))
 	return reader.ListObjects(ctx, filepath.Join(storage.GetPath(), path)+"/")
 }
 
