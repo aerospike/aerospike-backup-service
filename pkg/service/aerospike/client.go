260442136eb48a8011deec42e3f759b03ce87f1e
akorotkov <akorotkov@aerospike.com>
2025-08-12 21:54:26 +0300

    change everything to interfaces

---
+++ pkg/service/aerospike/client.go
@@ -0,0 +1,30 @@
+package aerospike
+
+import (
+	"context"
+
+	"github.com/aerospike/backup-go"
+)
+
+// Backuper exposes only backup-related methods.
+type Backuper interface {
+	Backup(ctx context.Context, config *backup.ConfigBackup, writer backup.Writer, reader backup.StreamingReader) (*backup.BackupHandler, error)
+	BackupXDR(ctx context.Context, config *backup.ConfigBackupXDR, writer backup.Writer) (*backup.HandlerBackupXDR, error)
+	InfoClient() backup.InfoGetter
+}
+
+// Restorer exposes only restore-related methods.
+type Restorer interface {
+	Restore(ctx context.Context, config *backup.ConfigRestore, streamingReader backup.StreamingReader) (backup.Restorer, error)
+	InfoClient() backup.InfoGetter
+}
+
+// Client combines all sub-interfaces for the Client's public API.
+type Client interface {
+	Backuper
+	Restorer
+
+	AerospikeClient() backup.AerospikeClient
+}
+
+var _ Client = (*backup.Client)(nil)
