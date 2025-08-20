ef1c3171214f87ae56bfe55ef92fe027b44e4ff6
akorotkov <akorotkov@aerospike.com>
2025-08-20 12:53:07 +0300

    add GET comment

--- pkg/util/routine_lock.go
+++ pkg/util/routine_lock.go
@@ -6,6 +6,8 @@ type LockMap struct {
 	locks sync.Map // map[string]*sync.Mutex
 }
 
+// Get returns the existing mutex for the key if present.
+// Otherwise, it creates, stores and returns new mutex.
 func (rl *LockMap) Get(key string) *sync.RWMutex {
 	actual, _ := rl.locks.LoadOrStore(key, &sync.RWMutex{})
 	return actual.(*sync.RWMutex)
