2eefa8f627eae5a17aec6e1fd47cf2de613d178a
akorotkov <akorotkov@aerospike.com>
2025-10-27 12:36:10 +0200

    main

--- cmd/backup/main.go
+++ cmd/backup/main.go
@@ -126,7 +126,7 @@ func initComponents(ctx context.Context, configFile string, remote bool) (
 		return nil, nil, nil, nil, fmt.Errorf("failed to create scheduler: %w", err)
 	}
 
-	pathService := service.NewPathService(config.ServiceConfig.GetBackupOrDefault().TimestampFormat)
+	pathService := service.NewPathService(config.ServiceConfig.GetBackupCommonOrDefault().TimestampFormat)
 	backendService := service.NewBackupBackendService(config, pathService)
 	registry := service.NewRunningBackupsRegistry(ctx, backendService, config)
 
