c0b7593b12bb522a08a6a4c00e0e208563e4159a
akorotkov <akorotkov@aerospike.com>
2025-11-05 18:54:59 +0200

    update version to 3.4.0

--- docs/docs.go
+++ docs/docs.go
@@ -3393,7 +3393,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "v3.3.1",
+	Version:          "v3.4.0",
 	Host:             "localhost:8080",
 	BasePath:         "",
 	Schemes:          []string{},
