398398ae0114660cf5f095ff9871e07770604e19
akorotkov <akorotkov@aerospike.com>
2026-03-16 22:47:39 +0200

    backup skipped error message

--- pkg/service/backup_routine_orchestrator.go
+++ pkg/service/backup_routine_orchestrator.go
@@ -16,7 +16,7 @@ import (
 )
 
 var (
-	errBackupSkipped   = errors.New("full backup skipped")
+	errBackupSkipped   = errors.New("backup skipped")
 	nonRetryableErrors = []error{asinfo.ErrNoNode}
 )
 
