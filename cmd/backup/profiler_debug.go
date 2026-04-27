56fdceb0bfd1bf506045ce5cb62ef9270d8a70f0
David Gerchikov <dgerchikov@aerospike.com>
2026-04-27 15:36:51 +0300

    Use http.Server with ReadHeaderTimeout for profiler

--- cmd/backup/profiler_debug.go
+++ cmd/backup/profiler_debug.go
@@ -3,9 +3,11 @@
 package main
 
 import (
+	"errors"
 	"log/slog"
 	"net/http"
 	_ "net/http/pprof"
+	"time"
 )
 
 func init() {
@@ -13,7 +15,11 @@ func init() {
 		// Binds to loopback only. In containers, access via: docker exec <id> curl localhost:6060/debug/pprof/
 		addr := "localhost:6060"
 		slog.Info("Starting profiler", "addr", addr)
-		if err := http.ListenAndServe(addr, nil); err != nil {
+		srv := &http.Server{
+			Addr:              addr,
+			ReadHeaderTimeout: 5 * time.Second,
+		}
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			slog.Error("Profiler stopped", "error", err)
 		}
 	}()
