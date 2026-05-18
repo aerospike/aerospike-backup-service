24bcd9c84859b7a822aa9604567e64fbecc9c5f3
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2026-05-18 13:53:08 +0300

    add max tries

--- pkg/model/config_default.go
+++ pkg/model/config_default.go
@@ -99,7 +99,7 @@ var StorageRetryPolicy = struct {
 var ScanRetryPolicy = &models.RetryPolicy{
 	BaseTimeout: 1000 * time.Millisecond,
 	Multiplier:  1.5,
-	MaxRetries:  10,
+	MaxRetries:  20,
 }
 
 var InfoRetryPolicy = &models.RetryPolicy{
