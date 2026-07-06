f3ac9ace119a8a3edf0312516fd9eb169ede8981
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2026-07-06 16:29:54 +0300

    add debug logs

--- pkg/service/history_manager.go
+++ pkg/service/history_manager.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/aerospike/aerospike-backup-service/v3/internal/attr"
 	"github.com/aerospike/aerospike-backup-service/v3/pkg/model"
@@ -34,21 +35,39 @@ func (hm *HistoryManagerImpl) FindLastRun(
 	ctx context.Context,
 	routine *model.BackupRoutine,
 ) (*model.BackupTime, error) {
+	start := time.Now()
+	fullStart := time.Now()
 	lastFullBackup, err := hm.backupReader.GetBackups(ctx, NewFullBackupFilter(routine).Last())
 	if err != nil {
 		return nil, fmt.Errorf("read last full backup failed: %w", err)
 	}
+	slog.Debug("Last full backup scan completed",
+		attr.Routine(routine.Name),
+		slog.Int("backups", len(lastFullBackup)),
+		slog.Duration("duration", time.Since(fullStart)),
+	)
 
 	if len(lastFullBackup) == 0 {
+		slog.Debug("Last backup time scan completed for routine",
+			attr.Routine(routine.Name),
+			slog.String("lastRun", model.NewNoBackupTime().String()),
+			slog.Duration("duration", time.Since(start)),
+		)
 		return model.NewNoBackupTime(), nil
 	}
 	lastFullTime := lastFullBackup[0].Created
 
+	incrementalStart := time.Now()
 	lastIncrBackup, err := hm.backupReader.GetBackups(ctx,
 		NewIncrementalBackupFilter(routine).WithFromTime(lastFullTime).Last())
 	if err != nil {
 		return nil, fmt.Errorf("read last incremental backup failed: %w", err)
 	}
+	slog.Debug("Last incremental backup scan completed",
+		attr.Routine(routine.Name),
+		slog.Int("backups", len(lastIncrBackup)),
+		slog.Duration("duration", time.Since(incrementalStart)),
+	)
 
 	var lastRun *model.BackupTime
 	if len(lastIncrBackup) > 0 {
@@ -59,7 +78,8 @@ func (hm *HistoryManagerImpl) FindLastRun(
 
 	slog.Debug("Last backup time scan completed for routine",
 		attr.Routine(routine.Name),
-		slog.String("lastRun", lastRun.String()))
+		slog.String("lastRun", lastRun.String()),
+		slog.Duration("duration", time.Since(start)))
 
 	return lastRun, nil
 }
