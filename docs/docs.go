8575189224a3a26a41168619bf4fb2c247240c8e
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2026-07-07 19:24:03 +0300

    BKRS-242 Remove hardcoded localhost (#574)
    remove hardocded localhost

--- docs/docs.go
+++ docs/docs.go
@@ -3593,7 +3593,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "v3.5.0",
-	Host:             "localhost:8080",
+	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Backup Service REST API Specification",
