447b865b8fc23d3a3158540255bf7a9057f87faf
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2026-07-07 19:17:08 +0300

    remove hardocded localhost

--- internal/server/handlers/info.go
+++ internal/server/handlers/info.go
@@ -6,7 +6,6 @@ package handlers
 // @description     Aerospike Backup Service
 // @license.name    Apache 2.0
 // @license.url     http://www.apache.org/licenses/LICENSE-2.0.html
-// @host            localhost:8080
 //
 // @externalDocs.description  OpenAPI
 // @externalDocs.url          https://swagger.io/resources/open-api/
