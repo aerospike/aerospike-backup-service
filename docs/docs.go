63579e80d899cb190701620f18d88d582b150ac1
David Gerchikov <dgerchikov@aerospike.com>
2026-08-13 12:48:02 +0300

    Set test version metadata

--- docs/docs.go
+++ docs/docs.go
@@ -3592,7 +3592,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "v3.6.1",
+	Version:          "v0.0.0-test-dgerchikov",
 	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},
--- internal/server/handlers/info.go
+++ internal/server/handlers/info.go
@@ -2,7 +2,7 @@ package handlers
 
 // Annotations to generate OpenAPI description (https://github.com/swaggo/swag)
 // @title           Backup Service REST API Specification
-// @version         v3.6.1
+// @version         v0.0.0-test-dgerchikov
 // @description     Aerospike Backup Service
 // @license.name    Apache 2.0
 // @license.url     http://www.apache.org/licenses/LICENSE-2.0.html
