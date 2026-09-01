b3057cbeaddd353719397ae3821ecc8512353198
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2026-09-01 14:32:23 +0300

    simplify test

--- test/integration/restore_timestamp_cumulative_test.go
+++ test/integration/restore_timestamp_cumulative_test.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/aerospike/aerospike-backup-service/v3/pkg/dto"
-	"github.com/aerospike/aerospike-backup-service/v3/pkg/model"
 )
 
 // TestRestoreByTimestampCumulativeWorkflow runs full backup, incremental backup,
@@ -21,19 +20,12 @@ func (s *BackupSuite) TestRestoreByTimestampCumulativeWorkflow() {
 	var fullBackup, incrBackup1, incrBackup2 dto.BackupDetails
 
 	s.Run("full", func() {
-		successCount := s.metricBackupSuccessEventCount(e, model.BackupTypeFull)
-
 		s.triggerFullBackup(e)
-
-		s.waitForMetricBackupSuccessEvent(e, model.BackupTypeFull, successCount)
 
 		fullBackup = s.waitForFullBackup(e)
 
 		s.assertBackupDetails(fullBackup, 0)
 		s.assertBackupListed(e, fullBackup)
-
-		s.assertMetricBackupSuccessEventCount(e, model.BackupTypeFull, successCount+1)
-		s.assertMetricLastSuccessfulBackup(e, model.BackupTypeFull, fullBackup.Created)
 	})
 
 	s.Run("incremental_1", func() {
@@ -45,17 +37,10 @@ func (s *BackupSuite) TestRestoreByTimestampCumulativeWorkflow() {
 
 		s.assertBackupDetails(incrBackup1, 1)
 		s.assertIncrementalBackupListed(e, incrBackup1)
-		s.GreaterOrEqual(incrBackup1.Created.Unix(), fullBackup.Created.Unix())
-
-		s.assertMetricLastSuccessfulBackup(e, model.BackupTypeIncremental, incrBackup1.Created)
+		s.Greater(incrBackup1.Created.Unix(), fullBackup.Created.Unix())
 	})
 
 	s.Run("incremental_2", func() {
-		// Use a different key by passing two elements, or write a custom insert.
-		// seedRecords uses the index as the key.
-		// So []int{0, 2} will insert key 0 with age 0, and key 1 with age 2.
-		// Wait, key 0 was already inserted with age 1.
-		// If we do []int{1, 2}, key 0 gets age 1, key 1 gets age 2.
 		s.seedRecords([]int{1, 2})
 
 		s.triggerIncrementalBackup(e)
@@ -65,14 +50,10 @@ func (s *BackupSuite) TestRestoreByTimestampCumulativeWorkflow() {
 
 		s.assertBackupDetails(incrBackup2, 2)
 		s.assertIncrementalBackupListed(e, incrBackup2)
-		s.GreaterOrEqual(incrBackup2.Created.Unix(), incrBackup1.Created.Unix())
-
-		s.assertMetricLastSuccessfulBackup(e, model.BackupTypeIncremental, incrBackup2.Created)
+		s.Greater(incrBackup2.Created.Unix(), incrBackup1.Created.Unix())
 	})
 
 	s.Run("restore_by_timestamp", func() {
-		successCount := s.metricRestoreSuccessEventCount(e)
-
 		s.Require().NoError(s.client.Truncate(nil, namespace, "", nil))
 
 		status := s.restoreByTimestamp(e, time.Now())
@@ -84,7 +65,5 @@ func (s *BackupSuite) TestRestoreByTimestampCumulativeWorkflow() {
 		s.Empty(status.Error)
 
 		s.assertRecordsRestored([]int{1, 2})
-
-		s.assertMetricRestoreSuccessEventCount(e, successCount+1)
 	})
 }
