bfe59a1f1d8299deb0a654e1125b031ad90df38a
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2026-09-24 13:42:22 +0300

    rate limit cnfig nil

--- pkg/dto/http_server_config.go
+++ pkg/dto/http_server_config.go
@@ -91,7 +91,7 @@ func (r *RateLimiterConfig) Validate() error {
 	if r.Tps != nil && *r.Tps <= 0 {
 		return errValidationNonPositive("tps", *r.Tps)
 	}
-	if r.Size == nil && *r.Size <= 0 {
+	if r.Size != nil && *r.Size <= 0 {
 		return errValidationNonPositive("size", *r.Size)
 	}
 	if err := validateUniqueNonEmpty("white-list", r.WhiteList); err != nil {
